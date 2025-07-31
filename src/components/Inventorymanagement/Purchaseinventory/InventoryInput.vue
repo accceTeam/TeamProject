f510c38a4dcd429e6a4dfb4b01cffac476318945
zimmer_Curry <zimmer_Curry@noreply.gitcode.com>
2025-07-31 15:15:24 +0800

    表格封装

---
+++ src/components/Inventorymanagement/Purchaseinventory/InventoryInput.vue
@@ -0,0 +1,115 @@
+<template>
+    <div>
+        <el-form :inline="true">
+            <el-form-item
+                v-for="item in inputConfigs"
+                :key="item.prop"
+                :label="item.label">
+
+                 <!-- 如果需要插槽渲染，则渲染插槽 -->
+                <template v-if="item.slotStatus">
+                    <slot :name="item.prop" ></slot>
+                </template>
+                  <el-input    
+                    v-else    
+                    :placeholder="item.placeholder"
+                    v-model="inputForm[item.prop]"
+                />
+                
+            </el-form-item>
+            <el-form-item>
+                <el-button :icon="Search" 
+                          
+                           type="primary"
+                           @click="handleSearch"
+                           >查询</el-button>
+                <el-button :icon="Refresh"
+                           @click="handleReset"
+                           >重置</el-button>
+                
+            </el-form-item>
+          
+        </el-form>
+    </div>
+</template>
+<script setup>
+import { ref,watch } from 'vue'
+import { Refresh, Search } from '@element-plus/icons-vue'
+
+const inputForm = ref({})
+
+// 定义向父组件发送数据  重置数据
+const emit = defineEmits(['searchInputData','clearSearchData'])
+
+// 子组件接受数据
+// 接收配置项
+const props = defineProps({
+  inputConfigs: {
+    type: Array,
+    default: () => []
+  }
+})
+
+
+
+
+// // 给input绑定
+// let inputFrom = ref({
+//     materialCode: '',
+//     materialName: ''
+// })
+
+// 搜索按钮  
+const handleSearch=()=>{
+    // // 执行查询操作
+    console.log(inputForm.value,'文本框中的内容')
+    // 通知父组件 父组件接收到
+    emit('searchInputData',inputForm.value)
+    // 清空数据
+    clearData()
+}
+// 重置按钮
+const handleReset=()=>{
+    props.inputConfigs.forEach(config => {
+    inputForm[config.prop] = ''
+    })
+    // 通知父组件 重置   这里没有传递数据，让父组件清空搜索的数据然后再请求一下就行
+    emit('clearSearchData')
+}
+// 清空数据
+// const clearData=()=>{
+//     inputFrom.value = {
+//         materialCode: '',
+//         materialName: ''
+//     }
+// }
+const clearData=()=>{
+  const form = {}
+  props.inputConfigs.forEach(config => {
+    form[config.prop] = ''
+  })
+  inputForm.value = form
+}
+
+// 使用watch监听props.inputConfigs初始化字段
+watch(
+  () => props.inputConfigs,
+  (newConfigs) => {
+    const form = {}
+    newConfigs.forEach(config => {
+      // 只提交非插槽的字段,插槽的字段数值在父组件中得到
+      if(!config.slotStatus){
+         form[config.prop] = ''
+      }
+    })
+    inputForm.value = form
+  },
+  { immediate: true } // 页面加载时立即执行
+)
+
+
+
+</script>
+<style setup>
+
+</style>
---
+++ src/views/erp/stock/PurInList.vue
@@ -0,0 +1,401 @@
+<template>
+    <div class="container">
+            <InventoryInput
+                @searchInputData='getSearchData'
+                @clearSearchData="resetSearchData"
+                :inputConfigs="inputConfigs"
+                >
+                <template #billDate_begin>
+                    <el-date-picker
+                      v-model="searchData.billDate_begin"
+                      type="date"
+                      placeholder="Pick a date"
+                      :default-value="new Date()"
+                       format="YYYY-MM-DD"
+                    />
+                </template>
+                <template #billDate_end>
+                    <el-date-picker
+                      v-model="searchData.billDate_end"
+                      type="date"
+                      placeholder="Pick a date"
+                      :default-value="new Date()"
+                       format="YYYY-MM-DD"
+                    />
+                </template>
+                <template #subject>
+                  <el-select v-model="searchData.subject" placeholder="Select" style="width: 240px">
+                    <el-option
+                      v-for="item in options"
+                      :key="item.value"
+                      :label="item.label"
+                      :value="item.value"
+                    />
+                  </el-select>
+                </template>
+                <template #billStage>
+                  <el-select v-model="searchData.billStage" placeholder="Select" style="width: 240px">
+                    <el-option
+                      v-for="item in options"
+                      :key="item.value"
+                      :label="item.label"
+                      :value="item.value"
+                    />
+                  </el-select>
+                </template>
+
+                <template #isEffective>
+                  <el-select v-model="searchData.isEffective" placeholder="Select" style="width: 240px">
+                    <el-option
+                      v-for="item in options"
+                      :key="item.value"
+                      :label="item.label"
+                      :value="item.value"
+                    />
+                  </el-select>
+                </template>
+
+                <template #isClosed>
+                  <el-select v-model="searchData.isClosed" placeholder="Select" style="width: 240px">
+                    <el-option
+                      v-for="item in options"
+                      :key="item.value"
+                      :label="item.label"
+                      :value="item.value"
+                    />
+                  </el-select>
+                </template>
+                <template #isVoided>
+                  <el-select v-model="searchData.isVoided" placeholder="Select" style="width: 240px">
+                    <el-option
+                      v-for="item in options"
+                      :key="item.value"
+                      :label="item.label"
+                      :value="item.value"
+                    />
+                  </el-select>
+                </template>
+            </InventoryInput>
+            <!-- 新增按钮 导出按钮... -->
+            <div>
+
+            </div>
+            <InventoryTable
+              ref="mytable" 
+              :tableData="tableData"
+              :tableSetting="tableSetting"
+              :tableTotal="tableTotal"
+              @updateSize="updateSize" 
+              @updateCurrentPage="updateCurrentPage"
+              @updateSort="updateSort"
+              >
+              <!-- 单据编号 -->
+            <template #billNo="{ scoped }">
+                <el-button
+                  type="primary"
+                  @click="lookDetail(scoped)"
+                  link>
+                {{ scoped.billNo}}
+                </el-button>
+            </template>
+            
+            <template #tableOperate>
+              <div style="display: flex;justify-content: center;">
+                <el-button 
+                 link
+                 size="small"
+                 type="primary"
+                 @click="editMditem(scoped)"
+                >编辑</el-button>
+              <el-button
+                 link
+                 size="small"
+                 type="primary"
+                 @click="deleteItem(scoped)"
+                >更多</el-button>
+              </div>
+            </template>
+            </InventoryTable>
+    </div>
+</template>
+<script setup>
+import { ElNotification } from 'element-plus'
+import { dayjs } from 'element-plus'
+import { ref } from 'vue';
+import InventoryInput from '/@/components/Inventorymanagement/Purchaseinventory/InventoryInput.vue';
+import InventoryTable from '/@/components/Inventorymanagement/Purchaseinventory/InventoryTable.vue';
+import { getPurchaseInventory } from '/@/api/InventoryManagement/purchaseInventory/purchaseInventory.js'
+
+// 定义input结构
+const inputConfigs = [
+  { label: '单据编号', placeholder: '请输入单据编号', prop: 'billNo', slotStatus:false},
+  { label: '单据日期', placeholder: '请输入开始时间', prop: 'billDate_begin',slotStatus:true},
+  {  placeholder: '请输入结束时间', prop: 'billDate_end',slotStatus:true}, 
+  { label: '单据主题', placeholder: '请输入单据主题', prop: 'subject', slotStatus:false},
+  { label: '供应商', placeholder: '请选择', prop: 'supplier', slotStatus:true},
+  { label: '单据阶段', placeholder: '请选择单据阶段', prop: 'billStage', slotStatus:true},
+  { label: '已生效', placeholder: '请选择是否已生效', prop: 'isEffective', slotStatus:true},
+  { label: '已关闭', placeholder: '请选择是否已关闭', prop: 'isClosed', slotStatus:true},
+  { label: '已作废', placeholder: '请选择是否已作废', prop: 'isVoided', slotStatus:true}
+]
+let tableData = ref([])
+const mytable = ref(null)
+// 右侧表格总条数、总页数  
+let tableTotal = ref(0)
+// 定义table结构
+const tableSetting = ref([
+    {
+        label:'单据编号',
+        prop:'billNo',
+        width:170,
+        slotStatus: true,
+        isFixed:true,
+        isSortTable:true
+    },
+    {
+        label:'单据日期',
+        prop:'billDate',
+        width:140, 
+        isSortTable:true
+    },
+    {
+        label:'单据主题',
+        prop:'specification',
+        width:140, 
+        isSortTable:true
+    },
+    {
+        label:'源单号',
+        prop:'srcNo',
+        width:160, 
+        isSortTable:true
+    },
+    {
+        label:'供应商',
+        prop:'supplierId_dictText',
+        width:140, 
+        isSortTable:true
+    },
+    {
+        label:'业务部门',
+        prop:'opDept_dictText',
+        width:140, 
+        isSortTable:true
+    },
+    {
+        label:'业务员',
+        prop:'operator_dictText',
+        width:140, 
+        isSortTable:true
+    },
+    {
+        label:'入库金额',
+        prop:'cost',
+        width:140, 
+    },
+    {
+        label:'结算金额',
+        prop:'settleAmt',
+        width:140, 
+    },
+    {
+        label:'已结算金额',
+        prop:'settledAmt',
+        width:140, 
+    },
+    {
+        label:'已开票金额',
+        prop:'invoicedAmt',
+        width:140, 
+    },
+    {
+        label:'发票类型',
+        prop:'invoiceType_dictText',
+        width:140, 
+    },
+    {
+        label:'入库经办',
+        prop:'handler_dictText',
+        width:140, 
+        isSortTable:true
+    },
+    {
+        label:'单据阶段',
+        prop:'billStage_dictText',
+        width:140, 
+    },
+    {
+        label:'已生效',
+        prop:'isEffective_dictText',
+        width:140, 
+    },
+    {
+        label:'已关闭',
+        prop:'isClosed_dictText',
+        width:140, 
+    },
+    {
+        label:'已作废',
+        prop:'isVoided_dictText',
+        width:140, 
+    },
+    {
+        label:'自动单据',
+        prop:'isAuto_dictText',
+        width:140, 
+    },
+    {
+        label:'红字单据',
+        prop:'isRubric_dictText',
+        width:140, 
+    },
+    {
+        label:'备注',
+        prop:'remark',
+        width:140, 
+    },
+    {
+        label:'生效时间',
+        prop:'effectiveTime',
+        width:140, 
+    },
+    {
+        label:'核批人',
+        prop:'createBy_dictText',
+        width:140, 
+    },
+    {
+        label:'制单时间',
+        prop:'billDate',
+        width:140, 
+    },
+    {
+        label:'制单部门',
+        prop:'sysOrgCode_dictText',
+        width:140, 
+    },
+    {
+        label:'修改时间',
+        prop:'updateTime',
+        width:140, 
+    },
+    {
+        label:'修改人',
+        prop:'createBy_dictText',
+        width:140, 
+    },
+    {
+        label:'操作',
+        prop:'tableOperate',
+        width:140, 
+        slotStatus: true,
+        isFixed:'right'
+    }
+])
+// 获取table所需的参数
+const needTableParams = ref({
+    // _t:new Date().getTime(),  //这是需要10位时间戳
+    _t: Math.floor(new Date().getTime() / 1000),
+    isVoided: 0,
+    column:'createTime',
+    order:'desc',
+    pageNo:1,
+    pageSize:10,
+    field:'id,,,billNo,billDate,subject,srcNo,supplierId_dictText,opDept_dictText,operator_dictText,cost,settleAmt,settledAmt,invoicedAmt,invoiceType_dictText,handler_dictText,billStage_dictText,isEffective_dictText,isClosed_dictText,isVoided_dictText,isAuto_dictText,isRubric_dictText,remark,effectiveTime,approver_dictText,createTime,createBy_dictText,sysOrgCode_dictText,updateTime,updateBy_dictText,action'
+})
+
+// 定义要搜索的数据
+const searchData = ref({
+    billNo: '',
+    billDate_begin: '',
+    billDate_end: '',
+    subject: '',
+    supplier: '',
+    billStage: '',
+    isEffective: '',
+    isClosed: '',
+    isVoided: '',
+})
+
+// 搜索
+const getSearchData=(data)=>{
+
+    // 处理时间数据--->>>年-月-日
+    const begin = searchData.value.billDate_begin
+    const end = searchData.value.billDate_end
+    const formatBegin = begin ? dayjs(begin).format('YYYY-MM-DD') : ''
+    const formatEnd = end ? dayjs(end).format('YYYY-MM-DD') : ''
+    searchData.value.billDate_begin = formatBegin
+    searchData.value.billDate_end = formatEnd
+
+    console.log(data,'子组件要搜索的数据');
+    console.log(searchData.value,'父组件搜索的数据');
+    // console.log(formatBegin,formatEnd,'日期'); 
+}
+// 排序
+const updateSort=({column,order})=>{
+    needTableParams.value.column = column
+    needTableParams.value.order = order
+    console.log(needTableParams.value,'needTableParams');
+    
+    getTableData()
+
+}
+
+// 获取table数据
+const getTableData = async () => {
+  // console.log(await getPurchaseInventory(needTableParams.value),'table数据');
+  try {
+    // countId,current,maxLimit,optimizeCountSql,orders,pages,records,searchCount,size,total
+    let {code,result} = await getPurchaseInventory(needTableParams.value)
+    if(code == 200){
+      ElNotification({
+        message:'采购入库数据获取成功',
+        type:'success',
+        duration: 1000,
+      })
+      let {countId,current,maxLimit,optimizeCountSql,orders,pages,records,searchCount,size,total} = result
+      tableData.value = records
+      tableTotal.value = total
+    }else{
+      ElNotification({
+        type:'error',
+        message:'采购入库数据获取失败'
+      })
+    }
+  } catch (error) {
+    console.log(error,'采购数据获取');
+    
+  }
+}
+getTableData()
+
+
+
+
+// 分页器
+// 分页器相关
+// 页码/条数更新
+const updateSize = (size) => {
+    console.log(size, 'size更新');
+    needTableParams.value.pageSize = size
+    // console.log(needTableParams.value,'测试一下');
+    // 更新数据
+    getTableData()
+}
+// 页码
+const updateCurrentPage = (page) => {
+    console.log(page, 'page更新');
+    needTableParams.value.pageNo = page
+    // 更新数据
+    // console.log('打印params',needTableParams.value);
+    getTableData()
+}
+
+</script>
+<style scoped>
+.container{
+    padding: 16px 24px;
+}
+
+</style>
