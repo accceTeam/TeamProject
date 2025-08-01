9ca36972f5f26a2c5634e972768097c8b63d0c88
lili <3172752794@qq.com>
2025-08-01 14:27:02 +0800

    genx

--- src/main.js
+++ src/main.js
@@ -7,10 +7,11 @@ import other from '/@/utils/other';
 
 import ElementPlus from 'element-plus';
 import '/@/theme/index.scss';
-
+import Antd from 'ant-design-vue';// 引入 Ant Design Vue
 const app = createApp(App);
 
 directive(app);
 other.elSvg(app);
 
-app.use(pinia).use(router).use(ElementPlus).mount('#app');
+app.use(pinia).use(router).use(ElementPlus).use(Antd).mount('#app');
+// app.use(pinia).use(router).use(ElementPlus).use(Antd).mount('#app');
